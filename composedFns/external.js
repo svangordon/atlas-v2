21b6dba61819b631c2ab44cc7b7c529d23666d18
Stephen Van Gordon <stephen.van.gordon@gmail.com>
2018-07-24 09:15:21 +0100

    split fn's, try to fix

---
+++ composedFns/external.js
@@ -0,0 +1,132 @@
+/*
+  Functions:
+    * displayClassification
+    * displayLandsat7SR
+    * getLabelLocations (getSamplingPoints)
+    * getTrainingInputs (sampleCollection)
+    * trainAlgorithm    (trainClassifier)
+*/
+
+function displayClassification(classificationImage, layerName) {
+  // Cast classificationImage to Image
+  classificationImage = ee.Image(classificationImage)
+
+  var atlasPalette = [
+    "8400a8", // Forest / Forêt
+    "8bad8b", // Savanna / Savane
+    "000080", // Wetland - floodplain / Prairie marécageuse - vallée inondable
+    "ffcc99", // Steppe / Steppe
+    "808000", // Plantation / Plantation
+    "33cccc", // Mangrove / Mangrove
+    "ffff96", // Agriculture / Zone de culture
+    "3366ff", // Water bodies / Plans d'eau
+    "ff99cc", // Sandy area / surfaces sableuses
+    "969696", // Rocky land / Terrains rocheux
+    "a87000", // Bare soil / Sols dénudés
+    "ff0000", // Settlements / Habitations
+    "ccff66", // Irrigated agriculture / Cultures irriguées
+    "a95ce6", // Gallery forest and riparian forest / Forêt galerie et formation ripicole
+    "d296e6", // Degraded forest / Forêt dégradée
+    "a83800", // Bowe / Bowé
+    "f5a27a", // Thicket / Fourré
+    "ebc961", // Agriculture in shallows and recession / Cultures des bas-fonds et de décrue
+    "28734b", // Woodland / Forêt claire
+    "ebdf73", // Cropland and fallow with oil palms / Cultures et jachère sous palmier à huile
+    "beffa6", // Swamp forest / Forêt marécageuse
+    "a6c28c", // Sahelian short grass savanna / Savane sahélienne
+    "0a9696", // Herbaceous savanna / Savane herbacée
+    "749373", // Shrubland / Zone arbustive
+    "505050", // Open mine / Carrière
+    "FFFFFF"  // Cloud / Nuage
+  ]
+  var atlasClasses = [1,2,3,4,6,7,8,9,10,11,12,13,14,15,21,22,23,24,25,27,28,29,31,32,78,99]
+  var remappedImage = classificationImage.remap(atlasClasses, ee.List.sequence(1, 26))
+  classificationImage = classificationImage.addBands(remappedImage)
+  Map.addLayer(classificationImage, {min:1, max:26, palette: atlasPalette, bands:'remapped'}, layerName)
+}
+
+/*
+  getLabelLocations
+    Create a collection of ~~center~~points at a given projection. Used for creating
+  the collection from which we will sample our basemap.
+
+  Parameters:
+    - zoneGeometry (ee.Geometry): The geometry within which we will create our collection of points.
+    - [projectionImage] (ee.Image, default: Atlas 2013): The image whose projection
+    we will use for the collection of points.
+  Returns:
+    ee.FeatureCollection (GeometryCollection)
+
+*/
+function getLabelLocations(zoneGeometry, projectionImage) {
+  zoneGeometry = ee.FeatureCollection(zoneGeometry).geometry()
+  projectionImage = projectionImage || ee.Image('users/svangordon/conference/atlas/swa_2013lulc_2km')
+  var projection = projectionImage.projection()
+  return ee.Image
+    .random()
+    .multiply(100000)
+    .toInt()
+    .reduceToVectors({
+      crs: projection,
+      geometry: zoneGeometry,
+      scale: projection.nominalScale(),
+      geometryInNativeProjection: true
+    })
+    .map(function(feature) {
+      var centroid = feature.centroid(5)
+      return centroid
+    })
+}
+
+/*
+  toPoint
+    Takes a feature with `longitude` and `latitude` properties, strips those properties,
+  and converts them into a geometry. Used so that features have geometries after a
+  `.sampleRegions` call.
+*/
+function toPoint(feature) {
+  // ? under the hood?
+  feature = ee.Feature(feature)
+  return ee.Feature(
+    ee.Geometry.Point([feature.get('longitude'), feature.get('latitude')]),
+    feature.toDictionary().remove(['longitude', 'latitude']))
+}
+
+function getTrainingInputs(basemap, labels, samplingGeometry, samplingScale) {
+  samplingScale = samplingScale || 30;
+  basemap = ee.Image(basemap)
+  labels = ee.Image(labels)
+  samplingGeometry = ee.FeatureCollection(samplingGeometry)
+
+  return basemap.addBands(labels)
+    .addBands(ee.Image.pixelLonLat())
+    .sampleRegions({
+      collection: samplingGeometry,
+      scale: samplingScale
+    })
+    .map(toPoint)
+}
+
+/*
+  Train algorithm. Returns a trained collection.
+*/
+function trainAlgorithm(inputData, trainingBands) {
+  var trainingSize = 0.7
+  inputData = inputData.randomColumn()
+  var trainingData = inputData.filter(ee.Filter.lt('random', trainingSize))
+  var testingData = inputData.filter(ee.Filter.gte('random', trainingSize))
+  var algorithm = ee.Classifier.randomForest(20)
+    .train(trainingData, 'b1', trainingBands)
+  var testingAccuracy = testingData.classify(algorithm).errorMatrix('b1', 'classification').accuracy()
+
+  return ee.Feature(null, {
+    accuracy: testingAccuracy,
+    algorithm: algorithm
+  })
+}
+
+exports.displayClassification = displayClassification;
+exports.getLabelLocations = getLabelLocations;
+exports.toPoint = toPoint;
+exports.getTrainingInputs = getTrainingInputs;
+exports.trainAlgorithm = trainAlgorithm;
--- composedFns/internal.js
+++ composedFns/internal.js
@@ -1,40 +1,9 @@
-function displayClassification(classificationImage, layerName) {
-  // Cast classificationImage to Image
-  classificationImage = ee.Image(classificationImage)
-
-  var atlasPalette = [
-    "8400a8", // Forest / Forêt
-    "8bad8b", // Savanna / Savane
-    "000080", // Wetland - floodplain / Prairie marécageuse - vallée inondable
-    "ffcc99", // Steppe / Steppe
-    "808000", // Plantation / Plantation
-    "33cccc", // Mangrove / Mangrove
-    "ffff96", // Agriculture / Zone de culture
-    "3366ff", // Water bodies / Plans d'eau
-    "ff99cc", // Sandy area / surfaces sableuses
-    "969696", // Rocky land / Terrains rocheux
-    "a87000", // Bare soil / Sols dénudés
-    "ff0000", // Settlements / Habitations
-    "ccff66", // Irrigated agriculture / Cultures irriguées
-    "a95ce6", // Gallery forest and riparian forest / Forêt galerie et formation ripicole
-    "d296e6", // Degraded forest / Forêt dégradée
-    "a83800", // Bowe / Bowé
-    "f5a27a", // Thicket / Fourré
-    "ebc961", // Agriculture in shallows and recession / Cultures des bas-fonds et de décrue
-    "28734b", // Woodland / Forêt claire
-    "ebdf73", // Cropland and fallow with oil palms / Cultures et jachère sous palmier à huile
-    "beffa6", // Swamp forest / Forêt marécageuse
-    "a6c28c", // Sahelian short grass savanna / Savane sahélienne
-    "0a9696", // Herbaceous savanna / Savane herbacée
-    "749373", // Shrubland / Zone arbustive
-    "505050", // Open mine / Carrière
-    "FFFFFF"  // Cloud / Nuage
-  ]
-  var atlasClasses = [1,2,3,4,6,7,8,9,10,11,12,13,14,15,21,22,23,24,25,27,28,29,31,32,78,99]
-  var remappedImage = classificationImage.remap(atlasClasses, ee.List.sequence(1, 26))
-  classificationImage = classificationImage.addBands(remappedImage)
-  Map.addLayer(classificationImage, {min:1, max:26, palette: atlasPalette, bands:'remapped'}, layerName)
-}
+var externalFunctions = require('users/svangordon/atelier:externalFunctions')
+var displayClassification = externalFunctions.displayClassification;
+var getLabelLocations = externalFunctions.getLabelLocations;
+var toPoint = externalFunctions.toPoint;
+var getTrainingInputs = externalFunctions.getTrainingInputs;
+var trainAlgorithm = externalFunctions.trainAlgorithm;
 
 /*
   getClassificationZone:
@@ -69,52 +38,9 @@ function getZonesBoundaries(boundaryGeometry, projectionImage) {
     })
 }
 
-/*
-  getSamplingPoints
-    Create a collection of ~~center~~points at a given projection. Used for creating
-  the collection from which we will sample our basemap.
 
-  Parameters:
-    - zoneGeometry (ee.Geometry): The geometry within which we will create our collection of points.
-    - [projectionImage] (ee.Image, default: Atlas 2013): The image whose projection
-    we will use for the collection of points.
-  Returns:
-    ee.FeatureCollection (GeometryCollection)
 
-*/
-function getSamplingPoints(zoneGeometry, projectionImage) {
-  zoneGeometry = ee.FeatureCollection(zoneGeometry).geometry()
-  projectionImage = projectionImage || ee.Image('users/svangordon/conference/atlas/swa_2013lulc_2km')
-  var projection = projectionImage.projection()
-  return ee.Image
-    .random()
-    .multiply(100000)
-    .toInt()
-    .reduceToVectors({
-      crs: projection,
-      geometry: zoneGeometry,
-      scale: projection.nominalScale(),
-      geometryInNativeProjection: true
-    })
-    .map(function(feature) {
-      var centroid = feature.centroid(5)
-      return centroid
-    })
-}
 
-/*
-  toPoint
-    Takes a feature with `longitude` and `latitude` properties, strips those properties,
-  and converts them into a geometry. Used so that features have geometries after a
-  `.sampleRegions` call.
-*/
-function toPoint(feature) {
-  // ? under the hood?
-  feature = ee.Feature(feature)
-  return ee.Feature(
-    ee.Geometry.Point([feature.get('longitude'), feature.get('latitude')]),
-    feature.toDictionary().remove(['longitude', 'latitude']))
-}
 
 /*
   getTimeFilter
@@ -192,45 +118,6 @@ function displayLandsat7SR(image, title) {
 }
 
 /*
-  =====
-  Sampling Images
-  =====
-*/
-
-function sampleCollection(basemap, labels, samplingGeometry, samplingScale) {
-  samplingScale = samplingScale || 30;
-  basemap = ee.Image(basemap)
-  labels = ee.Image(labels)
-  samplingGeometry = ee.FeatureCollection(samplingGeometry)
-
-  return basemap.addBands(labels)
-    .addBands(ee.Image.pixelLonLat())
-    .sampleRegions({
-      collection: samplingGeometry,
-      scale: samplingScale
-    })
-    .map(toPoint)
-}
-
-/*
-  Train algorithm. Returns a trained collection.
-*/
-function trainClassifier(inputData, trainingBands) {
-  var trainingSize = 0.7
-  inputData = inputData.randomColumn()
-  var trainingData = inputData.filter(ee.Filter.lt('random', trainingSize))
-  var testingData = inputData.filter(ee.Filter.gte('random', trainingSize))
-  var classifier = ee.Classifier.randomForest(20)
-    .train(trainingData, 'b1', trainingBands)
-  var testingAccuracy = testingData.classify(classifier).errorMatrix('b1', 'classification').accuracy()
-
-  return ee.Feature(null, {
-    accuracy: testingAccuracy,
-    classifier: classifier
-  })
-}
-
-/*
   Use this function to map over zones.
 */
 function classifyZone(classificationZone) {
@@ -258,27 +145,26 @@ function classifyZone(classificationZone) {
 
   // Create points that we will sample training image at. If using non-Atlas
   // data, change this to randomPoints
-  var samplingPoints = getSamplingPoints(classificationZone, labelImage)
-
-  var inputData = sampleCollection(trainingImage, labelImage, samplingPoints)
+  var trainingInputs = getTrainingInputs(classificationZone, labelImage)
+  var inputData = getTrainingInputs(trainingImage, labelImage, trainingInputs)
   var inputDataSize = ee.FeatureCollection(inputData).size()
-
-  // Uses a random forest by default. Change trainClassifier function if you
+  return inputData
+  // Uses a random forest by default. Change trainAlgorithm function if you
   // would like to adjust that.
-  var trainingResult = trainClassifier(inputData, trainingBands)
-  var classifier = trainingResult.get('classifier')
-  classifier = ee.Classifier(classifier)
-  var classifiedImage = trainingImage.classify(classifier)
+  var trainingResult = trainAlgorithm(inputData, trainingBands)
+  var algorithm = trainingResult.get('algorithm')
+  algorithm = ee.Classifier(algorithm)
+  var classifiedImage = trainingImage.classify(algorithm)
 
-  Export.image.toDrive({
-    image: classifiedImage,
-    folder: 'classifiedLulc',
-    region: classificationZone,
-    fileNamePrefix: 'atlasV2_' + classificationYear,
-    scale: 30,
-    description: 'atlasV2_' + classificationYear,
-    maxPixels: 1e13
-  });
+  // Export.image.toDrive({
+  //   image: classifiedImage,
+  //   folder: 'classifiedLulc',
+  //   region: classificationZone,
+  //   fileNamePrefix: 'atlasV2_' + classificationYear,
+  //   scale: 30,
+  //   description: 'atlasV2_' + classificationYear,
+  //   maxPixels: 1e13
+  // });
 
   return classifiedImage
 }
@@ -290,19 +176,18 @@ function classifyZone(classificationZone) {
 // Draw your own geometry
 // var geometry = geometry || ee.Geometry.Point(-4.285, 11.243)
 
-var zones = getZones(geometry)
-Map.addLayer(zone)
+var zones = getZonesBoundaries(geometry)
+Map.addLayer(zones)
 // Map.addLayer(geometry, {}, 'geometry')
 print(zones)
-// Map.addLayer(getSamplingPoints(zone))
-
-
-var classifiedImage = classifyZone(zone)
-displayClassification(classifiedImage)
 
 var classifiedZones = zones.map(classifyZone)
+print(classifiedZones)
+// var classifiedImage = ee.ImageCollection(classifiedZones).median()
+// displayClassification(classifiedImage, 'classifiedZones')
 
-Map.addLayer(classifiedZones)
+
+
 
 // var country = ee.FeatureCollection('users/svangordon/ecowas')
 //   .filter(ee.Filter.eq('NAME', 'Burkina Faso'))
@@ -314,6 +199,6 @@ Map.addLayer(classifiedZones)
 //
 // var labelImage = ee.Image('users/svangordon/conference/atlas/swa_2013lulc_2km')
 // var foo = zones.map(function(zone) {
-//   return getSamplingPoints(zone, labelImage)
+//   return getTrainingInputs(zone, labelImage)
 // })
 // Map.addLayer(foo, {}, 'sampling points')
